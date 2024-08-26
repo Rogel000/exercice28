699402be8b82d40e2ee751e02c35919688bf4ed5
Rogel000 <roge.lucie.dev@gmail.com>
2024-08-26 10:00:02 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-/* Créer la classe mère Vehicule avec les attributs et méthodes comme indiqué ci-dessus.
-o La méthode display retourne une chaine concaténant les attributs de l’objet
-Créer les class enfants Voiture et moto et pour chacune spécifier :
-o Les attributs spécifiques (s’il y en a)
-o Redéfinissez la méthode display en ajoutant « Auto : » ou « Moto : » avant de faire appel à la méthode display() du parent. Les attributs spécifiques doivent être ajoutés à la suite.
-Instancier une voiture et une moto et effectuer l’affichage du résultat dans la page HTML */
-
 import { Moto } from "./classe/moto.js";
 import { Voiture } from "./classe/voiture.js";
 
